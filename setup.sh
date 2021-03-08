3048108e0f7805c4cb67250051de0964cdadf3b9
Mayukh Dutta <mayukhere003@outlook.com>
2021-03-08 20:11:50 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir -p ~/.streamlit/
+echo "[general]
+email = \"mayukhere003@gmail.com\"
+" > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
